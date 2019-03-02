5b77db40dc2faa60db705afb82adc7ac47f6e2ee
roman <shmoop207@gmail.com>
2019-03-02 19:33:05 +0200

    first

--- config/modules/all.js
+++ config/modules/all.js
@@ -6,7 +6,7 @@ const logger_1 = require("@appolo/logger");
 module.exports = function (app) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         yield app.module(logger_1.LoggerModule);
-        yield app.module(new socket_1.SocketModule());
+        yield app.module(new socket_1.SocketModule({ socket: { transports: ['polling', 'websocket'] } }));
         yield app.module(new view_1.ViewModule({ viewEngine: view_1.ViewEngines.nunjucks }));
     });
 };
--- config/modules/all.ts
+++ config/modules/all.ts
@@ -6,6 +6,6 @@ import {LoggerModule} from '@appolo/logger';
 
 export = async function (app: App) {
     await app.module(LoggerModule);
-    await app.module(new SocketModule());
+    await app.module(new SocketModule({socket: {transports: ['polling', 'websocket']}}));
     await app.module(new ViewModule({viewEngine: ViewEngines.nunjucks}));
 }
